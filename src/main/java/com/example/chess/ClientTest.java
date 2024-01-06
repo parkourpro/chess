695d318da7c5b337461491e7baf95a7c14c9aab1
Nguyen Hai Son <haison0981@gmail.com>
2024-01-06 19:10:27 +0700

    "hide stage when move stage"

--- src/main/java/com/example/chess/ClientTest.java
+++ src/main/java/com/example/chess/ClientTest.java
@@ -1,4 +1,4 @@
-package com.example.javafxproject;
+package com.example.chess;
 
 import java.io.BufferedReader;
 import java.io.IOException;
--- src/main/java/com/example/chess/RegisterController.java
+++ src/main/java/com/example/chess/RegisterController.java
@@ -1,10 +1,11 @@
-package com.example.javafxproject;
+package com.example.chess;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -24,6 +25,8 @@ public class RegisterController {
 
     @FXML
     private PasswordField re_confpassword;
+    @FXML
+    private Button movetologinButton;
     @FXML
     private void register(ActionEvent event) {
         try {
@@ -45,23 +48,28 @@ public class RegisterController {
         }
     }
 //    @FXML
-//    void registerButton(ActionEvent event) throws IOException {
-//        String username = re_username.getText();
-//        String characterName = re_charactername.getText();
-//        String password = re_password.getText();
-//        String confPassword = re_confpassword.getText();
-//
-//        if (!password.equals(confPassword)) {
-//            // Display error message: Passwords do not match
-//        } else {
-//            // Register the user
-//            // If registration is successful, redirect to login page
-//            Stage stage = (Stage) re_username.getScene().getWindow();
-//            // replace 'Login.fxml' with your login page
-//            Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
-//            Scene scene = new Scene(root);
-//            stage.setScene(scene);
-//            stage.show();
-//        }
-//    }
+    public void movetoLogin(ActionEvent event) {
+        try {
+            // Load the FXML file for the registration window
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("login.fxml"));
+            Parent root = loader.load();
+
+            // Create a new stage for the registration window
+            Stage loginStage = new Stage();
+            loginStage.setScene(new Scene(root));
+            loginStage.show();
+
+            //get register stage
+            Stage registerStage = (Stage) movetologinButton.getScene().getWindow();
+
+            // Close register stage
+            registerStage.close();
+
+            // Show the registration window
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
