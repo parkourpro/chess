021cb9c394200ffd11063d4808906a3c3b1be172
trung-nc <trungnguyenitk65@gmail.com>
2024-02-01 07:06:54 +0700

    update

--- src/main/java/com/example/chess/chess/ChessMain.java
+++ src/main/java/com/example/chess/chess/ChessMain.java
@@ -2,7 +2,7 @@ package com.example.chess.chess;
 
 public class ChessMain
 {
-    public static void start(String[] args)
+    public static void start()
     {
         String[] arg = new String[0];
         ChessGame.startChess(arg);
